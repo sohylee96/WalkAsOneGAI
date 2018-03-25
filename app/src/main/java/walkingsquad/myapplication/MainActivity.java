457aeaa9a387d83d30c43a5053e1d49a727131fb
taylorhazlett <thazlett@bu.edu>
2018-03-25 12:43:10 -0400

    Initial commit

--- app/src/main/java/walkingsquad/myapplication/MainActivity.java
+++ app/src/main/java/walkingsquad/myapplication/MainActivity.java
@@ -2,12 +2,24 @@ package walkingsquad.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.content.Intent;
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+   @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_main);
     }
+
+    public void openLogInActivity (View view) {
+        Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+        startActivity(intent);
+    }
+
+    public void openSignUpActivity (View view2) {
+       Intent intent2 = new Intent(getApplicationContext(), Main3Activity.class);
+       startActivity(intent2);
+    }
+
 }
