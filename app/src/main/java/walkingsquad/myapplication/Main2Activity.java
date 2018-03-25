c7afe0b9d8282d33594fc3cacb88a6c871bc4458
taylorhazlett <thazlett@bu.edu>
2018-03-25 12:38:32 -0400

    Initial commit

--- app/src/main/java/walkingsquad/myapplication/Main2Activity.java
+++ app/src/main/java/walkingsquad/myapplication/Main2Activity.java
@@ -2,6 +2,7 @@ package walkingsquad.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main2Activity extends AppCompatActivity {
 
